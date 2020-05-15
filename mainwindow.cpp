cce7287e1c7abaf36b2099fb8e2d16dad522c2c0
Kristina <kristyaboldyreva@yandex.ru>
2020-05-15 15:33:12 +0700

    Таймер сделала на 1000

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -10,7 +10,7 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
     connect(&timer, SIGNAL(timeout()), this, SLOT(updateAll()));
-    timer.start(5000);
+    timer.start(1000);
 }
 
 void MainWindow:: updateAll()
